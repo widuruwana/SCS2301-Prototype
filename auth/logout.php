705b9420fb5fa71ee76d703dd3153ec7a3784da4
Kavindu Widyarathne <kavinduwidya@gmail.com>
2026-06-06 15:37:18 +0530

    Getting rid of old planned directory structure

--- auth/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/../core/session.php';
-require_once __DIR__ . '/../core/helpers.php';
-
-logout_user();
-flash('You have been signed out.', 'info');
-redirect('/SCS2301-Prototype/auth/login.php');
